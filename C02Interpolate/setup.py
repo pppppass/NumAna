dbfb69798f58b89d1e65efe6b42a8888592fbd70
pppppass <lzh2016p@pku.edu.cn>
2019-03-24 18:34:34 +0800

    Update Makefile

--- C02Interpolate/setup.py
+++ C02Interpolate/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 import numpy
 
-intp = Extension("intp", sources=["intp/algos.c", "intp/wrappers.c"], include_dirs=[numpy.get_include()], libraries=["m"], extra_compile_args=["-fopenmp", "-march=skylake"], extra_link_args=["-fopenmp"])
+intp = Extension("intp", sources=["intp/algos.c", "intp/wrappers.c"], include_dirs=[numpy.get_include()], libraries=["m"], extra_compile_args=["-march=skylake"])
 
 setup(ext_modules=[intp])
